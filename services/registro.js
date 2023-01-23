edd453aa9eac6da33c564a141b422dee59daa67f
Raphael Serafim <68257896+raphaelvserafim@users.noreply.github.com>
2023-01-23 15:46:58 -0400

    Update registro.js

--- services/registro.js
+++ services/registro.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
 /**
- * ### Avalia o dominio no registro.br
+ * Evaluates the domain on the registro.br website
  * @see https://registro.br/dominio/regras/
- * @param {string} domain
+ * @param {string} domain - The domain to evaluate
+ * @returns {Promise} - A promise that resolves to the availability of the domain
+ * @throws {Error} - If the domain is not a string or is not in the correct format
  * @author Guilherme Neves <guilhermeasn@yahoo.com.br>
  */
 export default function getRegistroBrAvail(domain) {
-  if (
-    typeof domain !== 'string' ||
-    !domain.length ||
-    /[^A-Za-z0-9àáâãéêíóôõúüç.-]/.test(domain)
-  )
-    throw new Error('O domínio não foi informado corretamente!');
-  return axios.get(`https://registro.br/v2/ajax/avail/raw/${domain}`);
+  if (typeof domain !== 'string' || !domain.length) {
+    throw new Error('The domain must be a non-empty string');
+  }
+
+  if (!/^[A-Za-z0-9àáâãéêíóôõúüç.-]+$/.test(domain)) {
+    throw new Error('The domain contains invalid characters');
+  }
+
+  const url = `https://registro.br/v2/ajax/avail/raw/${domain}`;
+
+  return axios.get(url);
 }
